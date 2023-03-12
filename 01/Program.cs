24255c9fbe4a1f8724733e38faedf4aedc644c8a
Валерия Алексеевна <valeriaalekseevna@MacBook-Pro-Valeria.local>
2023-03-12 13:55:17 +0300

    задача

---
+++ 01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        Console.Write("Введите количество строк: ");
+        int n = int.Parse(Console.ReadLine());
+
+        string[] arr = new string[n];
+        for (int i = 0; i < n; i++) {
+            Console.Write($"Введите строку {i + 1}: ");
+            arr[i] = Console.ReadLine();
+        }
+
+        string[] result = new string[n];
+        int j = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i].Length <= 3) {
+                result[j] = arr[i];
+                j++;
+            }
+        }
+
+        Console.WriteLine("Массив строк, длина которых меньше или равна 3:");
+        for (int i = 0; i < j; i++) {
+            Console.WriteLine(result[i]);
+        }
+    }
+}
